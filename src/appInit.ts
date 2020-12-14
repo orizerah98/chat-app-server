9f1a7723e539ffb37feec17c95255a6b76713b86
Ori Zerah <orizerah@Oris-MacBook-Pro.local>
2020-12-14 14:25:48 +0200

    App initiation contains passport config, cors, routing and more

---
+++ src/appInit.ts
@@ -0,0 +1,49 @@
+import { Application, Response } from "express";
+import router from "./api/routing";
+import { json, urlencoded } from "body-parser";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "./db/models/user";
+
+const enableCors: any = (req: Request, res: Response, next: Function) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+};
+
+export const initPassport = (app: Application) => {
+  app.use(
+    require("express-session")({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+};
+
+const setLocalAuth = (): void => {
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+};
+
+const initApp = (app: Application) => {
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+  initPassport(app);
+  setLocalAuth();
+  app.use(enableCors);
+  app.use(router);
+};
+
+export default initApp;
