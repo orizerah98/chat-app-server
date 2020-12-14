482b66f18317944057a02685e51be1f37cc0f952
Ori Zerah <orizerah@Oris-MacBook-Pro.local>
2020-12-14 14:27:06 +0200

    using initApp

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import * as http from "http";
+import initMongoose from "./db/mongoose";
+import initApp from "./appInit";
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
+
+initMongoose();
+initApp(app);
 
 server.listen(port, () => {
   console.log(`listening on port ${port}`);
