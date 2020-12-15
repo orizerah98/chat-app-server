18cef85d4d7630597646632f84569bf65568071c
Ori Zerah <orizerah@Oris-MacBook-Pro.local>
2020-12-15 14:40:41 +0200

    Fixed login

--- src/api/controllers/authController.ts
+++ src/api/controllers/authController.ts
@@ -6,6 +6,10 @@ import User from "../../db/models/user";
 export const registerUser: IExpressRouteCallback = async (req, res) => {
   try {
     const { email, password, displayName } = req.body;
+    if (!email || !password) {
+      res.status(400).send("Missing email or password");
+      return;
+    }
     const user = await authService.registerUser(email, password, displayName);
     passport.authenticate("local");
     res.send(user);
@@ -16,16 +20,24 @@ export const registerUser: IExpressRouteCallback = async (req, res) => {
 };
 
 export const login: IExpressRouteCallback = async (req, res) => {
-  const { email, password } = req.body;
-  const user = new User({ email: email, password: password });
-  req.login(user, (err) => {
-    if (err) {
-      console.log(err);
-      res.status(401).send(err);
-    } else {
-      passport.authenticate("local")(req, res, () => {
-        res.send(user);
-      });
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).send("Missing email or password");
+      return;
     }
-  });
+    const user = new User({ email: email, password: password });
+    req.login(user, (err) => {
+      if (err) {
+        console.log(err);
+        res.status(401).send(err);
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.send(user);
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
